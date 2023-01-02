663ff30781f037288b60af3ad17451ad1249cdf8
Ben <bengrandy98@gmail.com>
2023-01-02 06:26:38 -0500

    Added support for editting a profile page once successfully logged into a user.

--- Login/users.js
+++ Login/users.js
@@ -1,6 +1,6 @@
 var users = {
-    'Ben': {username: 'Ben', password: '123456', name: 'Ben', address: '123 Street', email: '123@gmail.com', phoneNum: '123-456-7891'}, 
-    'Temel': {username: 'Temel', password: 'Fish', name: 'Temel', address: 'Bitch Avenue', email: 'Temel@gmail.com', phoneNum: '420-420-4200'},
-    'George': {username: 'George', password: 'Jungle', name: 'George', address: 'Tree Jungle', email: 'George@gmail.com', phoneNum: 'Too Lazy' },
-    'Admin': {username: 'Admin', password: 'Administator', name: 'Admin', address: 'NULL', email: 'Admin@gmail.com', phoneNum: '696-969-6969' }
+    Ben: {username: 'Ben', password: '123456', name: 'Ben', address: '123 Street', email: '123@gmail.com', phoneNum: '123-456-7891'}, 
+    Temel: {username: 'Temel', password: 'Fish', name: 'Temel', address: 'Bitch Avenue', email: 'Temel@gmail.com', phoneNum: '420-420-4200'},
+    George: {username: 'George', password: 'Jungle', name: 'George', address: 'Tree Jungle', email: 'George@gmail.com', phoneNum: 'Too Lazy' },
+    Admin: {username: 'Admin', password: 'Administator', name: 'Admin', address: 'NULL', email: 'Admin@gmail.com', phoneNum: '696-969-6969' }
 }
